b302ecc31b8c40856e0b1b46711223ef28ca09d2
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2025-03-30 21:45:33 +0100

    composer

---
+++ app/src/main/java/com/example/mycomposeapp/CounterActivity.kt
@@ -0,0 +1,80 @@
+package com.example.mycomposeapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
+
+class CounterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyComposeAppTheme {
+                CounterScreenPreview()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterScreenPreview() {
+    MyComposeAppTheme {
+        CounterScreen()
+    }
+}
+
+@Composable
+fun CounterScreen() {
+    var resetState by remember { mutableStateOf(0) }
+    Log.d("CounterScreen", "state updated")
+    if (resetState < 5) {
+        var count by remember { mutableStateOf(0) }
+        Log.d("CounterScreen", "count state executed: $count")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            Arrangement.Center,
+            Alignment.CenterHorizontally
+        ) {
+            Text("Current count is: $count")
+            Button(onClick = {
+                count++
+                resetState++
+            }, modifier = Modifier.padding(10.dp)) {
+                Text("Click me")
+            }
+        }
+    } else {
+        Log.d("CounterScreen", "state reset ")
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            Arrangement.Center,
+            Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { resetState = 0 }, modifier = Modifier.padding(10.dp)) {
+                Text("Reset")
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -6,20 +6,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +40,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyComposeAppTheme {
-                MainScrenn()
+//                MainScrenn()
+                ModifierDemo()
             }
         }
     }
@@ -38,24 +51,92 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     MyComposeAppTheme {
-        MainScrenn()
+        ModifierDemo()
     }
 }
 
 @Composable
-fun MainScrenn() {
+fun MainScreenBox() {
     val context = LocalContext.current
 
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-        Text("Hello Nick")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 12.dp)
+    ) {
+        Button(onClick = {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }, modifier = Modifier.align(Alignment.CenterStart)) { Text("Click CenterStart") }
+
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null,
+            contentDescription = null, modifier = Modifier.align(Alignment.Center)
         )
         Button(onClick = {
             Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
-        }) { Text("Click Me") }
+        }, modifier = Modifier.align(Alignment.CenterEnd)) { Text("Click CenterEnd") }
+
+        Button(onClick = {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }, modifier = Modifier.align(Alignment.TopCenter)) { Text("Click TopCenter") }
+        Button(onClick = {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }, modifier = Modifier.align(Alignment.BottomCenter)) { Text("Click BottomCenter") }
     }
 
+
+}
+
+@Composable
+fun ModifierDemo() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())) {
+        val boxModifier = Modifier
+            .weight(1f)
+            .padding(10.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .size(200.dp, 200.dp)
+        Row() {
+            for (int in 1..4) {
+                Box(
+                    modifier = boxModifier
+                        .background(Color.Red)
+                ) { }
+            }
+        }
+        Row() {
+            for (int in 1..4) {
+                Box(
+                    modifier = boxModifier
+                        .background(Color.Yellow)
+                ) { }
+            }
+        }
+        Row() {
+            for (int in 1..4) {
+                Box(
+                    modifier = boxModifier
+                        .background(Color.Green)
+                ) { }
+            }
+        }
+        Row() {
+            for (int in 1..4) {
+                Box(
+                    modifier = boxModifier
+                        .background(Color.Blue)
+                ) { }
+            }
+        }
+        Row() {
+            for (int in 1..4) {
+                Box(
+                    modifier = boxModifier
+                        .background(Color.Black)
+                ) { }
+            }
+        }
+    }
 
 }
