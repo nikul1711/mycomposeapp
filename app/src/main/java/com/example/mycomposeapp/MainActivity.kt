aec6c6f807dea877474569a67400b606fcfe1e61
Nikul Boharwal <75658868+nikul1711@users.noreply.github.com>
2025-04-03 10:08:26 +0100

    error

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.mycomposeapp
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,7 +24,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -38,77 +35,88 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
+import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.Alignment
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             MyComposeAppTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "/main") {
-                    composable("/main") {
-                        MainScreenForNavigation(navController)
-                    }
-                    composable("/second") {
-                        SecondScreenForNavigation(navController)
-                    }
+                val navController = rememberNavController() // Create NavController
 
-                    // define other screens here if needed in the future
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") { HomeScreen(navController) }
+                    composable("second") { SecondScreen(navController) }
                 }
             }
         }
     }
 }
 
+@Composable
+fun HomeScreen(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(modifier = Modifier.align(Alignment.Center), onClick = {
+            navController.navigate("second")
+        }) {
+            Text("Go to Second Screen")
+        }
+    }
+
+}
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Button(onClick = { navController.navigate("second") }) {
+        Text(" Second Screen")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposeAppTheme {
-        MainScreenForNavigation(rememberNavController())
+        HomeScreen(rememberNavController())
     }
+
 }
 
 @Composable
 fun MainScreenForNavigation(navController: NavHostController) {
-    val listSize = 5;
+    val listSize = 5
     val listColumns = List(listSize) { "Hello Android $it" }
     val listRow = List(listSize) { "Hello Nick $it" }
-    val list3 = List(listSize) { "Android v.$it" }
-    Column {
-        LazyItemHeader("Android Developer")
-        LazyRow(modifier = Modifier.height(110.dp)) {
-            items(list3) { item ->
-                LazyListItemData(item, onClick = {
-                    navController.navigate("/second")
-                })
-            }
+
+    LazyColumn {
+        item {
+            LazyItemHeader("Hello Android Developer")
         }
-        LazyColumn() {
-            item {
-                LazyItemHeader("Hello Android Developer")
-            }
-            items(listColumns) { item ->
-                LazyListItemData(item, onClick = {
-                    navController.navigate("/second")
-                })
-            }
-            item {
-                LazyItemHeader("User Developer")
-            }
-            items(listRow) { item ->
-                LazyListItemData(item, onClick = {
-                    navController.navigate("/second")
-                })
-            }
+        items(listColumns) { item ->
+            LazyListItemData(item, onClick = {
+                // Call navigate on the NavController when item is clicked
+                navController.navigate("second")
+            })
+        }
+        item {
+            LazyItemHeader("User Developer")
+        }
+        items(listRow) { item ->
+            LazyListItemData(item, onClick = {
+                navController.navigate("second")
+            })
         }
     }
+
 }
 
 @Composable
@@ -135,7 +143,7 @@ fun LazyListItemData(item: String, onClick: (data: String) -> Unit) {
             .height(80.dp)
             .padding(12.dp)
             .fillMaxWidth()
-            .clickable { onClick(item) }
+
     ) {
         Image(
             painter = painterResource(R.drawable.ic_close_24),
@@ -145,13 +153,31 @@ fun LazyListItemData(item: String, onClick: (data: String) -> Unit) {
                 .width(16.dp)
                 .align(Alignment.TopEnd)
         )
-        Text(text = item, modifier = Modifier.align(Alignment.CenterStart))
+        Text(
+            text = item,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .clickable { onClick(item) },
+        )
     }
 }
 
 @Composable
 fun SecondScreenForNavigation(navController: NavHostController) {
+    Column {
+        Text(
+            text = "Second Screen",
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        )
+    }
 }
+
 
 @Composable
 fun ThirdScreenForNavigation() {
